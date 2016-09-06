8644473056ae4e6b8d51e5032e1890aefe741eb1
Alex Gleason <alex@alexgleason.me>
2016-09-06 18:45:46 -0400

    Fix artist URLs

--- monkeywagtail/artist/wagtail_hooks.py
+++ monkeywagtail/artist/wagtail_hooks.py
@@ -1,5 +1,5 @@
 from wagtail.contrib.modeladmin.options import (
-    ModelAdmin, ModelAdminGroup, modeladmin_register)
+    ModelAdmin, modeladmin_register)
 from .models import Artist
 
 
@@ -9,10 +9,10 @@ class ArtistAdmin(ModelAdmin):
     menu_icon = 'fa-bolt'  # change as required
     menu_order = 300  # will put in 3rd place (000 being 1st, 100 2nd)
     add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
-    list_display = ('artist_name', 'date_formed', 'artist_image')
+    list_display = ('title', 'date_formed', 'artist_image')
     # https://docs.djangoproject.com/en/1.8/ref/contrib/admin/#django.contrib.admin.ModelAdmin.list_display
     list_filter = ('date_formed',)
-    search_fields = ('artist_name',)
+    search_fields = ('title',)
 
 
 modeladmin_register(ArtistAdmin)
