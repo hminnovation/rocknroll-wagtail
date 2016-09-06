d85fabd8e1615b6b07c5739668fbe3bab0ddd4d3
Edd Baldry <edd.baldry@gmail.com>
2016-09-06 08:30:49 +0100

    Add new tab ReviewPage for album song details

--- monkeywagtail/core/blocks.py
+++ monkeywagtail/core/blocks.py
@@ -1,13 +1,13 @@
 from wagtail.wagtailcore import blocks
 from wagtail.wagtailimages.blocks import ImageChooserBlock
-#from wagtail.wagtaildocs.blocks import DocumentChooserBlock
-#from wagtail.wagtailembeds.blocks import EmbedBlock
+# from wagtail.wagtaildocs.blocks import DocumentChooserBlock
+# from wagtail.wagtailembeds.blocks import EmbedBlock
 from django.utils.encoding import force_text
 from django.utils.html import format_html_join, mark_safe
-#from wagtailblocks_cards.blocks import CardsBlock
+# from wagtailblocks_cards.blocks import CardsBlock
 
 
-#class CardBlock(blocks.StructBlock):
+# class CardBlock(blocks.StructBlock):
 # @TODO decide if this actually needs to be here.
 #    image = ImageChooserBlock(required=False)
 #    title = blocks.CharBlock()
@@ -15,8 +15,42 @@ from django.utils.html import format_html_join, mark_safe
 #    cta_page = blocks.PageChooserBlock(required=False)
 
 
+class SongStreamBlock(blocks.StreamBlock):
+    # Title name. Time.
+    SongBlock = blocks.StructBlock([
+        ('album_song', blocks.CharBlock(blank=True, required=False, label='e.g. Waiting Room')),
+        ('album_song_length', blocks.TimeBlock(blank=True, required=False)),
+    ], title="Song", icon="fa-music", template="blocks/songstreamblock.html")
+
+
+class StreamBlock(blocks.StreamBlock):
+    paragraph = blocks.RichTextBlock(
+        icon="pilcrow",
+        template="blocks/paragraph.html"
+    )
+    header = blocks.CharBlock(
+        classname="title",
+        icon="fa-header",
+        template="blocks/h3.html"
+    )
+    image = blocks.StructBlock([
+        ('image', ImageChooserBlock()),
+        ('caption', blocks.CharBlock(blank=True, required=False)),
+        ('style', blocks.ChoiceBlock(choices=[
+            ('', 'Select an image size'),
+            ('full', 'Full-width'),
+            ('half', 'Half-width')
+        ], required=False))
+    ], icon="image", template="blocks/image.html")
+    blockquote = blocks.StructBlock([
+        ('text', blocks.TextBlock()),
+        ('attribute_name', blocks.CharBlock(blank=True, required=False, label='e.g. Guy Picciotto')),
+        ('attribute_group', blocks.CharBlock(blank=True, required=False, label='e.g. Fugazi')),
+    ], icon="openquote", template="blocks/blockquote.html")
+
+
 class StandardBlock(blocks.StreamBlock):
-# This class was originally writted by Alex Gleason (@alexgleason)
+    # This class was originally writted by Alex Gleason (@alexgleason)
     paragraph = blocks.RichTextBlock(
         icon="pilcrow",
         template="blocks/paragraph.html"
@@ -53,7 +87,7 @@ class StandardBlock(blocks.StreamBlock):
     blockquote = blocks.StructBlock([
         ('text', blocks.TextBlock()),
         ('attribute_name', blocks.CharBlock(blank=True, required=False, label='e.g. Guy Picciotto')),
-        ('attribute_group', blocks.CharBlock(blank=True,required=False, label='e.g. Fugazi')),
+        ('attribute_group', blocks.CharBlock(blank=True, required=False, label='e.g. Fugazi')),
     ], icon="openquote", template="blocks/blockquote.html")
 
 #    def render_basic(self, value):
