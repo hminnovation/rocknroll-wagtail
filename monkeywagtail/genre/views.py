eb917b71f4f873c9d03c64ef9232a72bb7551447
Edd Baldry <edd.baldry@gmail.com>
2016-09-12 16:50:56 +0100

    Fix views error for genres

--- monkeywagtail/genre/views.py
+++ monkeywagtail/genre/views.py
@@ -3,14 +3,14 @@ from .models import GenreClass
 
 
 def genre_list(request):
-    genres = Genre.objects.all()
+    genres = GenreClass.objects.all()
     return render(request, 'genre/genre_list.html', {
         'genres': genres,
     })
 
 
 def genre_detail(request, slug):
-    genre = get_object_or_404(Genre, slug=slug)
+    genre = get_object_or_404(GenreClass, slug=slug)
     return render(request, 'genre/genre_detail.html', {
         'genre': genre,
     })
