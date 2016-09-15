630133393c75351f14322e7b042c62cc0376f8b6
Edd Baldry <edd.baldry@gmail.com>
2016-09-15 11:44:17 +0100

    Add reverse URL to author detail page from reviews

--- monkeywagtail/author/urls.py
+++ monkeywagtail/author/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^authors/$', views.author_list),
+    url(r'^authors/$', views.author_list, name='authors'),
     url(r'^authors/(?P<slug>[-\w]+)/$', views.author_detail),
 ]
