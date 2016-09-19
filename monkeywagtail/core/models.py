26b1037a98fdaea54cd5aa30c39dc8f6924510a7
Edd Baldry <edd.baldry@gmail.com>
2016-09-19 23:10:47 +0100

    Pep-8 models.py and add detail on Andy Babic menu

--- monkeywagtail/core/models.py
+++ monkeywagtail/core/models.py
@@ -16,28 +16,29 @@ from monkeywagtail.core.blocks import ChildMenuBlock, ParentMenuBlock, MenuBlock
 # This isn't standard Wagtail behaviour, which is to use the inbuilt `show-in-
 # menu` flag on each page. If you wanted to use that you can implement it using
 # templatetags. The best place to look for that is https://github.com/torchbox/
-# wagtaildemo/blob/master/demo/templatetags/demo_tags.py to look directly at 
+# wagtaildemo/blob/master/demo/templatetags/demo_tags.py to look directly at
 # code. There's also an excellent tutorial at http://www.tivix.com/blog/
 # working-with-wagtail-menus/
 #
 # We can't use that standard page based paradigm (that uses TreeBeard) since we
-# don't have a simple hierarchical tree relationship between content (e.g. 
+# don't have a simple hierarchical tree relationship between content (e.g.
 # parent <-> child <-> grandchild) and are using generic models for author,
-# album, artist and genre. 
+# album, artist and genre.
 #
 # The menu here uses a StreamField block (based on two StructBlocks) that we de-
 # fined in core/blocks.py. We pull that through as StreamField(MenuBlock())
 # This works great if you just want a JSON output of the menu (since it's a
-# StreamField object). 
+# StreamField object).
 
 # Note if you need a version that's based around fields then Jordi Joan has an
 # awesome implementation that he blogged about at http://jordijoan.me/
-# simple-orderable-menus-wagtail/
+# simple-orderable-menus-wagtail/ or Andy Babic has a pip installable menu
+# module that is even more awesome at https://github.com/rkhleics/wagtailmenus
 
 
 class MainMenu(ClusterableModel):
-    title = models.CharField(max_length=255, null=False, blank=False, 
-        verbose_name='Menu title')
+    title = models.CharField(
+        max_length=255, null=False, blank=False, verbose_name='Menu title')
     menu = StreamField(MenuBlock(), verbose_name="Menus", blank=True)
 
     panels = [
@@ -52,9 +53,10 @@ class MainMenu(ClusterableModel):
         verbose_name = "Main navigation"
         verbose_name_plural = "Main navigation"
 
+
 class FooterMenu(ClusterableModel):
-    title = models.CharField(max_length=255, null=False, blank=False, 
-        verbose_name='Menu title')
+    title = models.CharField(
+        max_length=255, null=False, blank=False, verbose_name='Menu title')
     menu = StreamField(MenuBlock(), verbose_name="Menus", blank=True)
 
     panels = [
@@ -72,9 +74,11 @@ class FooterMenu(ClusterableModel):
 # than class name e.g. 'primarynav': {random_title}.objects.order_by('order'),
 # that way we could just have a single menu class
 
+
 # Related pages
 class RelatedPage(Orderable, models.Model):
-    page = models.ForeignKey('wagtailcore.Page', null=True, blank=True, 
+    page = models.ForeignKey(
+        'wagtailcore.Page', null=True, blank=True,
         on_delete=models.SET_NULL, related_name='+')
 
     class Meta:
