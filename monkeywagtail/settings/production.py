1ee6cc5a35fe05c9e51cd730cc7e2b5d8c2a50da
Edd Baldry <edd.baldry@gmail.com>
2016-10-05 08:20:28 +0100

    Change back app_name

--- monkeywagtail/settings/production.py
+++ monkeywagtail/settings/production.py
@@ -34,7 +34,7 @@ for key, value in os.environ.items():
 
 # Basic configuration
 
-APP_NAME = env.get('APP_NAME', 'rocknrollwagtail')
+APP_NAME = env.get('APP_NAME', 'monkeywagtail')
 
 if 'SECRET_KEY' in env:
     SECRET_KEY = env['SECRET_KEY']
@@ -74,7 +74,6 @@ if 'MEDIA_DIR' in env:
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {'default': dj_database_url.config()}
-
 
 else:
     DATABASES = {
@@ -151,7 +150,7 @@ LOGGING = {
         }
     },
     'loggers': {
-        'rocknrollwagtail': {
+        'monkeywagtail': {
             'handlers':     [],
             'level':        'INFO',
             'propagate':    False,
@@ -181,14 +180,14 @@ LOGGING = {
 
 if 'LOG_DIR' in env:
     # monkey log
-    LOGGING['handlers']['rocknrollwagtail_file'] = {
+    LOGGING['handlers']['monkeywagtail_file'] = {
         'level':        'INFO',
         'class':        'cloghandler.ConcurrentRotatingFileHandler',
-        'filename':     os.path.join(env['LOG_DIR'], 'rocknrollwagtail.log'),
+        'filename':     os.path.join(env['LOG_DIR'], 'monkeywagtail.log'),
         'maxBytes':     5242880,  # 5MB
         'backupCount':  5
     }
-    LOGGING['loggers']['wagtail']['handlers'].append('rocknrollwagtail_file')
+    LOGGING['loggers']['wagtail']['handlers'].append('monkeywagtail_file')
 
     # Wagtail log
     LOGGING['handlers']['wagtail_file'] = {
