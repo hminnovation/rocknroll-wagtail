308557f0b44d742a48e3e5e079f8bfd7cec00cf8
Edd Baldry <edd.baldry@gmail.com>
2016-10-06 22:04:19 +0100

    Semi-hardcoded breadcrumb in place across site

--- monkeywagtail/album/urls.py
+++ monkeywagtail/album/urls.py
@@ -5,7 +5,6 @@ from . import views
 # @TODO
 # Decide if we _really_ do want to allow albums be directly
 # accessible...
-app_name = 'album'
 urlpatterns = [
     url(r'^albums/$', views.album_list, name='albums'),
     url(r'^albums/(?P<slug>[-\w]+)/$', views.album_detail),
--- monkeywagtail/genre/urls.py
+++ monkeywagtail/genre/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^genres/$', views.genre_list),
+    url(r'^genres/$', views.genre_list, name='genres'),
     url(r'^genres/(?P<slug>[-\w]+)/$', views.genre_detail),
     # below doesn't work. But is getting closer to it...
     url(r'^genres/(?P<slug>[-\w]+)/(?P<sub_genre_slug>[-\w]+)/$', views.subgenre_detail),
