664b371074680c0dab56611b80530c4f4da66ba7
Edd Baldry <edd.baldry@gmail.com>
2016-10-31 17:38:59 +0000

    Paginate authors

--- monkeywagtail/author/views.py
+++ monkeywagtail/author/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Author
 
 
 def author_list(request):
     authors = Author.objects.all()
+
+    paginator = Paginator(authors, 2)  # Show 2 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        authors = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        authors = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        authors = paginator.page(paginator.num_pages)
+
     return render(request, 'author/author_list.html', {
-        'authors': authors,
+         'authors': authors,
     })
+    # For more details on pagination
+    # https://docs.djangoproject.com/en/1.10/topics/pagination/
 
 
 def author_detail(request, slug):
